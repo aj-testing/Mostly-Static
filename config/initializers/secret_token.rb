e25c4e17433543130af0590245c72dc22391dc62
Aaron Jefferson Villanueva <terror_collie@yahoo.com>
2012-01-15 08:28:55 -0800

    First Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MostlyStatic::Application.config.secret_token = 'a14fca3ebcc0cd1051660e5cd224c6533315bef7cf6c13aa6c834a281c7c308ac9850654d33805b4216e54dbdc19ab04e37b964392965d1e4548dd14a1c8f7a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MostlyStatic::Application.config.session_store :cookie_store, key: '_mostly_static_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MostlyStatic::Application.config.session_store :active_record_store
